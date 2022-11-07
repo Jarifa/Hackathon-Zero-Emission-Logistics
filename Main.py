6a65403a4f148d014d658b5c2028f43f76f00db6
Jarifa <davevdschouw@gmail.com>
2022-11-07 14:14:51 +0100

    new df test

--- Main.py
+++ Main.py
@@ -23,13 +23,12 @@ api = KaggleApi()
 api.authenticate()
 
 # importing datasets from API
-api.dataset_download_files('surajjha101/fortune-top-1000-companies-by-revenue-2022', unzip=True)
-df_2022 = pd.read_csv('Fortune 1000 Companies by Revenue.csv')
-api.dataset_download_files('shivamb/fortune-global-2000-companies-till-2021', unzip=True)
-df_2021 = pd.read_csv('fortune_2000_in_2021.csv')
+api.dataset_download_files('surajjha101/arianazmoudeh/airbnbopendata', unzip=True)
+df_original = pd.read_csv('Airbnb_Open_Data.csv')
+
 
 print('test')
-st.write(df_2021)
+st.write(df_original)
 
 # Achtergrond streamlit
 achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
