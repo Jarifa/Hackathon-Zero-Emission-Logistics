2216b2a9cca17a187899971500cbf76873bb9584
Jarifa <davevdschouw@gmail.com>
2022-11-07 14:50:55 +0100

    price en service fee als int64

--- Introductie.py
+++ Introductie.py
@@ -16,7 +16,7 @@ st.write(df_original)
 st.write(df_original.dtypes)
 
 st.write("price en service fee als int64")
-df = df_original.apply(lambda row: row['price'][1,:])
+df = df_original.apply(lambda row: row['price'][1:])
 st.write(df)
 
 # Achtergrond streamlit achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
