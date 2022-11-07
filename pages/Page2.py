bdcae95165453a88b3fc6da8cc1176d7d4972087
Jarifa <davevdschouw@gmail.com>
2022-11-07 15:27:36 +0100

    test folium

--- pages/Page2.py
+++ pages/Page2.py
@@ -9,9 +9,12 @@ st.markdown('Hierbij de plattegrond van New York met de parameters van de elke A
 mb = folium.Map(location=[40.730610, -73.935242], tiles="Openstreetmap")
 
 data = pd.read_csv('clean_df.csv')
-for i in range(0, len(data)):
-    folium.marker([data.iloc[i]['lat'], data.iloc[i]['lon']],
-                  popup=data.iloc[i]['name'], ).add_to(mb)
 
+"""for i in range(0, len(data)):
+    folium.marker([data.iloc[i]['lat'], data.iloc[i]['lon']],
+                  popup=data.iloc[i]['name']).add_to(mb)"""
+
+data.apply(lambda row: folium.marker([row['lat'], row['lon']],
+                                     popup=row['name']).add_to(mb))
 
 st.write(mb)
