137d3f3dbca89a4b7e03b3293ea6686e399fa6bd
Jarifa <davevdschouw@gmail.com>
2022-11-07 14:49:01 +0100

    new df test

--- Introductie.py
+++ Introductie.py
@@ -1,5 +1,5 @@
 # importing required libraries and packages
-
+import numpy as np
 import pandas as pd
 import streamlit as st
 from kaggle.api.kaggle_api_extended import KaggleApi
@@ -15,6 +15,9 @@ st.write('Original "Dirty" dataframe:')
 st.write(df_original)
 st.write(df_original.dtypes)
 
+st.write("price en service fee als int64")
+df = df_original.apply(lambda row: row['price'][1,:])
+st.write(df)
 
 # Achtergrond streamlit achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
 
