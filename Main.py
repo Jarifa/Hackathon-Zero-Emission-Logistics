8a8295b0e35bf17785a3cc2c0221b220ad109e51
Jarifa <davevdschouw@gmail.com>
2022-11-07 13:14:32 +0100

    authentication.py deleted, enviroment variables nu ingeladen in streamlit

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#importing required libraries and packages
+# importing required libraries and packages
 import streamlit as st
 import requests
 import pandas as pd
@@ -6,13 +6,12 @@ import numpy as np
 import json
 import os
 
-import authentication
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
 
-#importing datasets from API
+# importing datasets from API
 api.dataset_download_files('surajjha101/fortune-top-1000-companies-by-revenue-2022', unzip=True)
 df_2022 = pd.read_csv('Fortune 1000 Companies by Revenue.csv')
 api.dataset_download_files('shivamb/fortune-global-2000-companies-till-2021', unzip=True)
