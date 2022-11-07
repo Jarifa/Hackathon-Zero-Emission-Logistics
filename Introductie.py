9900cebdadbd3dea507b76d080bae13e358fb0b2
Jarifa <davevdschouw@gmail.com>
2022-11-07 14:42:15 +0100

    new df test

--- Introductie.py
+++ Introductie.py
@@ -1,22 +1,7 @@
 # importing required libraries and packages
-from datetime import datetime
 
-import streamlit as st
-import requests
 import pandas as pd
-import numpy as np
-import json
-import os
-
-import geopandas as gpd
-import seaborn
-import plotly.express as px
-import plotly.figure_factory as ff
-from shapely.geometry import Point
-import missingno as msno
-import statsmodels.api as sm
-from fuzzywuzzy import fuzz
-
+import streamlit as st
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
@@ -26,8 +11,10 @@ api.authenticate()
 api.dataset_download_files('arianazmoudeh/airbnbopendata', unzip=True)
 df_original = pd.read_csv('Airbnb_Open_Data.csv')
 
+st.write('Original "Dirty" dataframe:')
 st.write(df_original)
+st.write(df_original.dtypes)
 
-# Achtergrond streamlit
-#achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
+
+# Achtergrond streamlit achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
 
