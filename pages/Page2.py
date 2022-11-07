4b4c740162521c7aadc2bf406f8be005008958aa
Jarifa <davevdschouw@gmail.com>
2022-11-07 15:31:54 +0100

    test folium

--- pages/Page2.py
+++ pages/Page2.py
@@ -10,11 +10,21 @@ mb = folium.Map(location=[40.730610, -73.935242], tiles="Openstreetmap")
 
 data = pd.read_csv('clean_df.csv')
 
+#range:
 """for i in range(0, len(data)):
     folium.marker([data.iloc[i]['lat'], data.iloc[i]['lon']],
                   popup=data.iloc[i]['name']).add_to(mb)"""
 
-data.apply(lambda row: folium.marker([row['lat'], row['lon']],
-                                     popup=row['name']).add_to(mb))
+#iterrows:
+for index, row in data.iterrows():
+    folium.Marker([row['lng'], row['lat']], popup=row['NAME']).add_to(mb)
+
+# apply:
+"""data.apply(lambda row: folium.marker([row['lat'], row['lon']],
+                                     popup=row['name']).add_to(mb))"""
 
 st.write(mb)
+
+
+
+
