d0bc6e44baafee00070aea90cd5d88ebff8b39b5
Jarifa <davevdschouw@gmail.com>
2022-11-07 14:17:02 +0100

    new df test

--- Main.py
+++ Main.py
@@ -30,7 +30,7 @@ df_original = pd.read_csv('Airbnb_Open_Data.csv')
 print('test')
 st.write(df_original)
 
-# Achtergrond streamlit
+#  Achtergrond streamlit
 achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
 
 
