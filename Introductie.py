36f8f72f4fbab6b26d3a56b6f7df00d507c906ca
Jarifa <davevdschouw@gmail.com>
2022-11-07 14:55:36 +0100

    axis = 1 op apply

--- Introductie.py
+++ Introductie.py
@@ -14,11 +14,9 @@ df_original = pd.read_csv('Airbnb_Open_Data.csv')
 st.write('Original "Dirty" dataframe:')
 st.write(df_original)
 st.write(df_original.dtypes)
-st.write(df_original.columns)
-st.write(df_original['price'])
 
 st.write("price en service fee als int64")
-df = df_original.apply(lambda row: row['price'][1:])
+df = df_original.apply(lambda row: row['price'][1:], axis = 1)
 st.write(df)
 
 # Achtergrond streamlit achtergrond = ''' <style> body { background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366"); background-size: cover; } </style> '''
