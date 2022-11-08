c36967195c4c0d532fa7f0569a44352456579d40
Jarifa <davevdschouw@gmail.com>
2022-11-08 16:26:20 +0100

    page 3

--- pages/3_dataset_analyse.py
+++ pages/3_dataset_analyse.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 import numpy as np
-import missingno as msno
+import plotly as px
+import streamlit as st
 
 
 ##########Code voor regression
@@ -24,5 +25,13 @@ def add_bg_from_url():
 
 add_bg_from_url()
 #######################################################################################################################
+##Price tegenover bouwjaar
+df = pd.read_csv('clean_df.csv')
+st.write(df.columns)
+# fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+
+
+####Correlation service fee
+
 
 data = df.to_csv('clean_df.csv')
