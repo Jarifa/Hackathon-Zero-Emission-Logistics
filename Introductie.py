8a7d8a5f446855b2cb6edcba056bb22edde04b13
Dave <davevdschouw@gmail.com>
2022-11-08 15:03:13 +0100

    fix for NaN values before convert to int

--- Introductie.py
+++ Introductie.py
@@ -21,15 +21,12 @@ df.dropna(subset=['lat', 'long'], inplace=True)
 st.write('Na values na drop:', df[['lat', 'long']].isna().sum())
 
 # price and service fee cleanup
-st.write('waarden in price: ', df['price'].value_counts())
-
 df['price'] = df['price'].astype(str).str[1:]
 df['service fee'] = df['service fee'].astype(str).str[1:]
 df['price'] = df['price'].apply(lambda row: row.replace(',', ''))
 df['service fee'] = df['service fee'].apply(lambda row: row.replace(',', ''))
 
-st.write('waarden in price: ', df['price'].value_counts())
-
+df['price'] = df['price'].apply(lambda price: np.nan if price == 'an' else price)
 df = df.astype({"price": "int", "service fee": "int"})
 
 st.write(df)
