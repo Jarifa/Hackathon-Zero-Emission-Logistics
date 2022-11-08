8ff003d35b8f22c1b91b82c6325761a3d9324193
Jarifa <davevdschouw@gmail.com>
2022-11-08 16:46:11 +0100

    plotly chart

--- pages/3_dataset_analyse.py
+++ pages/3_dataset_analyse.py
@@ -33,7 +33,7 @@ fig1 = px.line(data, x="Construction year", y="price", color="neighbourhood grou
 # AttributeError: 'Figure' object has no attribute 'savefig'
 # st.header("**Enkele dataset analyses**")
 # st.markdown("")
-st.line_chart(fig1)
+st.plotly_chart(fig1)
 
 ####Correlation service fee
 
