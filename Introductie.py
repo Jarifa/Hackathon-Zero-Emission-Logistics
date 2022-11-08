4dd66d051836eadfea4b53a004c9d4013456e9ba
Jarifa <Davevdschouw@gmail.com>
2022-11-08 14:42:40 +0100

    convert price en service kolom naar int test

--- Introductie.py
+++ Introductie.py
@@ -24,13 +24,13 @@ st.write('Na values na drop:', df[['lat', 'long']].isna().sum())
 # price and service fee cleanup
 df['price'] = df['price'].str[1:]
 df['service fee'] = df['service fee'].str[1:]
-df['price'] = df['price'].apply(lambda i, i.replace(',', ''), axis = 1)
-df['service fee'] = df['service fee'].replace(',', '')
+df['price'] = df['price'].apply(lambda row: row.replace(',', ''), axis=1)
+df['service fee'] = df['service fee'].apply(lambda row: row.replace(',', ''), axis=1)
 
-value = df['price'][19]
-st.write('test value = ', value)
-value = value.replace(',', '')
-st.write('test value after replace = ', value)
+# value = df['price'][19]
+# st.write('test value = ', value)
+# value = value.replace(',', '')
+# st.write('test value after replace = ', value)
 
 # df = df.astype({"price": "int",
 #                "service fee": "int"})
@@ -47,19 +47,15 @@ st.write(df)
 # Achtergrond############################################################################################
 def add_bg_from_url():
     st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://th.bing.com/th/id/R.c6d89244cbb1c6d916e87acd4984c09e?rik=5PX6aQALkMW6vg&riu=http%3a%2f%2fimg06.deviantart.net%2fca80%2fi%2f2014%2f196%2fb%2f1%2fminimal_new_york_by_kevichan-d61zr27.jpg&ehk=k9sZxjAJ%2blmBlAiGwtw0kJmHNRzbU4GLxez%2f42eOMo4%3d&risl=&pid=ImgRaw&r=0");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
+        f""" <style> .stApp {{ background-image: url(
+        "https://th.bing.com/th/id/R.c6d89244cbb1c6d916e87acd4984c09e?rik=5PX6aQALkMW6vg&riu=http%3a%2f%2fimg06
+        .deviantart.net%2fca80%2fi%2f2014%2f196%2fb%2f1%2fminimal_new_york_by_kevichan-d61zr27.jpg&ehk=k9sZxjAJ
+        %2blmBlAiGwtw0kJmHNRzbU4GLxez%2f42eOMo4%3d&risl=&pid=ImgRaw&r=0"); background-attachment: fixed; 
+        background-size: cover }} </style> """,
+        unsafe_allow_html=True
+    )
+
 
 add_bg_from_url()
 ###############################################################################################################
 df.to_csv('clean_df.csv')
-
