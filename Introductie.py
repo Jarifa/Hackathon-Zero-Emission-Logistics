930cf179677a56c0517da4773a79414f830d0ed9
Jarifa <davevdschouw@gmail.com>
2022-11-10 16:27:49 +0100

    dataframe verwijderd bij kaart

--- Introductie.py
+++ Introductie.py
@@ -28,8 +28,8 @@ api = KaggleApi()
 api.authenticate()
 
 ###Introtekst
-st.header("DASHBOARD Dave van der Schouw, Benjamin Niemann")
-st.markdown("Welkom bij het dashboard over AirBnB data van NewYork.")
+st.title("DASHBOARD Dave van der Schouw, Benjamin Niemann")
+st.header("Welkom bij het dashboard over AirBnB data van NewYork.")
 st.image('Kaggle.png')
 
 # importing datasets from API
